5d048f5d344d763dcc81dc2b3c1744d2b4ff6cd5
freedemons <646714507@qq.com>
2019-12-16 15:52:31 +0800

    no message

--- server.js
+++ server.js
@@ -13,23 +13,27 @@ let users = [{
   }
 ]
 let home = [{
-      id: 'h01',
-      name: '菜单1',
-      {
-        id: 'h02',
-        name: '菜单2'
-      }]
-    const server = http.createServer(function (req, res) {
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      console.log('req.url:', req.url);
-      if (req.url === '/api/users') {
-        res.end(JSON.stringify(users));
-      } else {
-        res.end('Not Found');
-      }
+    id: 'h01',
+    name: '菜单1'
+  },
+  {
+    id: 'h02',
+    name: '菜单2'
+  }
+]
+const server = http.createServer(function (req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  console.log('req.url:', req.url);
+  if (req.url === '/api/users') {
+    res.end(JSON.stringify(users));
+  } else if (req.url === '/api/home') {
+    res.end(JSON.stringify(home));
+  } else {
+    res.end('Not Found');
+  }
 
-    });
+});
 
-    server.listen(3000, () => {
-      console.log('后端API接口服务器已经启动在3000端口');
-    });
+server.listen(3000, () => {
+  console.log('后端API接口服务器已经启动在3000端口');
+});
